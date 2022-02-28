bec74b2ecf524638d55eac7d5ff8ab87990da85f
Gulshan Kumar <inbox.gulshan@outlook.com>
2022-02-28 19:36:21 +0530

    removed empty folders

--- test.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-const e = React.createElement;
-const domContainer = document.querySelector('#test');
-
-ReactDOM.render(e("h1", {id: "recipe-0", 'data-type': "title"}, "Baked Salmon"), domContainer);
